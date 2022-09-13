9ecfdf525c2dd10db588a950e541959bf283a9c9
Robert Sheffield <sheffira2@gmail.com>
2022-09-12 17:39:33 -0700

    Trying to sequelize constructor setup

--- sequelize/index.js
+++ sequelize/index.js
@@ -7,15 +7,12 @@ console.log(process.env.DATABASE_HOST)
 console.log(process.env.DATABASE_URI)
 console.log(process.env.DATABASE_USER)
 console.log(process.env.DATABASE_PASSWORD)
-const sequelize = new Sequelize(
-    process.env.NODE_ENV !== 'production' ? 'tutoring-portal-2' : process.env.DATABASE, 
-    process.env.NODE_ENV !== 'production' ? 'postgres' : process.env.DATABASE_USER, 
-    process.env.NODE_ENV !== 'production' ? "Rkf7010zaqxsw!@#" : process.env.DATABASE_PASSWORD, 
-    {
-        host: process.env.NODE_ENV !== 'production' ?  'localhost': process.env.DATABASE_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URI, {
+        // host: process.env.NODE_ENV !== 'production' ?  'localhost': process.env.DATABASE_HOST,
         dialect: 'postgres',
         dialectOptions: {
             ssl: {
+                require: true,
                 rejectUnauthorized: false
             }
         },
